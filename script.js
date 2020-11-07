8712ae70ff46704d39ddece11f48204cda117d8d
Matheus Acácio <matheusacacio@unigranrio.br>
2020-11-07 02:40:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const card = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+})
